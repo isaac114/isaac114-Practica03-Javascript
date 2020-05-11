c8f396d631de818bd8938bf0e7a2797cac2e1911
isaac114 <51967948+isaac114@users.noreply.github.com>
2020-05-11 17:51:21 -0500

    Validacion de las cedulas

--- formulario/formulario.js
+++ formulario/formulario.js
@@ -1,7 +1,7 @@
 var validarCedula = function(){
     document.getElementsByName("cedula")[0].value;
     var cedula = String(document.getElementsByName("cedula")[0].value);
-    var cedula2 = cedula.split("");
+
     if(isNaN(cedula)){
         alert('Por Favor en el campo cedula ingresa solo numeros')
     }
@@ -10,4 +10,37 @@ var validarCedula = function(){
 
 var validarNombre = function(){
     var nombre = String(document.getElementsByName("nombre")[0].value);
+    var cadena = nombre.split(" ");
+    var tamano = cadena.length;
+    
+    if(tamano > 2){
+        alert('Solo se permiten 2 Nombres')
+    }else if(tamano < 2){
+        alert('Solo se permiten 2 Nombres')
+    }else {
+        var nombre1 = cadena[0].split("");
+        var nombre2 = cadena[1].split("");
+        var validador = false;
+        for(var i=0; i<=nombre1.length; i++){
+                
+            if(isNaN(nombre1[i])===false){
+                validador = true;
+            }
+        }
+        for(var i=0; i<=nombre2.length; i++){
+                
+            if(isNaN(nombre2[i])===false){
+                validador = true;
+            }
+        }
+
+        if(validador===true){
+            alert("No se permiten numeros en los nombres");
+        }
+    }
+
+    
+    
+
+    
 }

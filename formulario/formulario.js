df9be392b7dcbd3f2df9e706791479974561f994
isaac114 <51967948+isaac114@users.noreply.github.com>
2020-05-11 21:54:46 -0500

    Validacion de Contrasenas

--- formulario/formulario.js
+++ formulario/formulario.js
@@ -110,18 +110,88 @@ var validarTelefono = function(){
 
 var validarFecha = function() {
     var elemento = document.getElementsByName("fecha")[0];
-    if(elemento.value.length > 9) {
+    if(elemento.value.length == 10) {
         var dia = elemento.value[0]+elemento.value[1];
         var mes = elemento.value[3]+elemento.value[4];
         var ano = elemento.value[6]+elemento.value[7]+elemento.value[8]+elemento.value[9];
 
         console.log(ano);
 
-        if( ((dia < 1)||(dia > 31)) || (elemento.value[2] != "/") || ((mes < 1) || (mes > 12)) || (elemento.value[5] != "/") || ((ano < 0) || (ano > 2020) || (ano > 1900)) ) {
+        if( ((dia < 1)||(dia > 31)) || (elemento.value[2] != "/") || ((mes < 1) || (mes > 12)) || (elemento.value[5] != "/") || ((ano < 0) || (ano > 2020) || (ano < 1900)) ) {
             alert('Formato Incorrecto');
         }
-    } else if (elemento.value.length > 10) {
-        alert('Formato Incorrecto');
+    } else if(elemento.value.length > 10) {
+        alert('Formato Incorrectoa');
 
+    }
+}
+
+
+var validarCorreo = function() {
+    var nombre = String(document.getElementsByName("correo")[0].value);
+    var cadena = nombre.split("@");
+    if((cadena[0] != " ") && cadena[1] != null ){
+        
+        var parte1 = cadena[0].split("");
+        console.log(parte1.length);
+
+        if(parte1.length >= 3){
+            var parte2 = cadena[1];
+            var dominio1 = "ups.edu.ec";
+            var dominio2 = "est.ups.edu.ec";
+            if((parte2 === dominio1)||(parte2 === dominio2)){
+
+            }else{
+                if(parte2.length > 9){
+                    alert("Se nececita un dominio de la forma ups.edu.ec o est.ups.edu.ec")
+                }
+            }
+
+        }else{
+            alert("Formato no compatible. Se nececita minimo 3 caracteres antes del @");
+        }
+
+    }
+}
+
+
+
+
+
+
+var validarContrasena = function() {
+    var el = document.getElementsByName("psw")[0].value;
+    var elemento = el.split("");
+    if(elemento.length > 0) {
+        if(elemento.length >= 8) {		
+            var mayuscula = false;
+            var minuscula = false;
+            var numero = false;
+            var caracter_especial = false;
+            for(var i = 0;i<elemento.length;i++) {
+                if(el.charCodeAt(i) >= 65 && el.charCodeAt(i) <= 90) {
+                    mayuscula = true;
+                } else if(el.charCodeAt(i) >= 97 && el.charCodeAt(i) <= 122) {
+                    minuscula = true;
+                } else if(el.charCodeAt(i) >= 48 && el.charCodeAt(i) <= 57) {
+                    numero = true;
+                } else if(el.charCodeAt(i) == 64 || el.charCodeAt(i) == 95 || el.charCodeAt(i) == 36) {
+                    caracter_especial = true;
+                }  else {
+                    return true;
+                }
+            }
+            if(mayuscula == true && minuscula == true && caracter_especial == true && numero == true) {
+                
+            } else {
+                alert('Su contrasena debe tener: Numeros, Letras Mayusculas y minusculas y caracteres especiales');
+                
+            }
+        } else {
+            
+            alert('contrasena demasiado Corta');
+        }
+    } else {
+        alert('No hay Contrasena');
     }
 }
